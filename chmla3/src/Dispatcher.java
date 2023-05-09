2a6c3f30d505cd89472294f61730f0ae04bb80b8
nazardyda <59512293+nazardyda@users.noreply.github.com>
2023-05-09 23:14:08 +0300

    Update Dispatcher.java

--- chmla3/src/Dispatcher.java
+++ chmla3/src/Dispatcher.java
@@ -1,47 +1,82 @@
+import java.util.Arrays;
+
 public class Dispatcher {
+    public static double rightSolve(int n) {
+        double h = 1.0 / (n - 1);
+        double[] a = new double[n];
+        double[] b = new double[n];
+        double[] c = new double[n];
+        double[] f = new double[n];
+        double[] alpha = new double[n];
+        double[] beta = new double[n];
+        double[] y = new double[n];
+        double[] x = new double[n];
+        double[] yx = new double[n];
+
+        Arrays.fill(y, 0);
+        Arrays.fill(alpha, 0);
+        Arrays.fill(beta, 0);
+        Arrays.fill(x, 0);
+        Arrays.fill(yx, 0);
+
+        a[0] = 0;
+        b[n - 1] = 0;
+        c[0] = 1 + 2 / (h * h);
+        c[n - 1] = 1 + 2 / (h * h);
+        f[0] = 0;
+        f[n - 1] = 0.5;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && i < n - 1) {
+                a[i] = 1 / (h * h) + (1 + i * h) / (2 * h);
+                b[i] = 1 / (h * h) - (1 + i * h) / (2 * h);
+                c[i] = 1 + 2 / (h * h);
+                f[i] = 2 / ((1 + h * i) * (1 + h * i) * (1 + h * i));
+            }
+        }
+
+        alpha[0] = 0;
+        beta[0] = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            double dilnyk = a[i] * alpha[i] + c[i];
+            alpha[i + 1] = -b[i] / dilnyk;
+            beta[i + 1] = (f[i] - a[i] * beta[i]) / dilnyk;
+        }
+
+        y[n - 1] = (f[n - 1] - a[n - 1] * beta[n - 1]) / (c[n - 1] + a[n - 1] * alpha[n - 1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+            y[i] = alpha[i + 1] * y[i + 1] + beta[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            x[i] = i * h;
+            yx[i] = (i == 0 || i == n - 1) ? y[i] : 1 / (x[i] + 1);
+        }
+
+        double yMinY = 0;
+
+        for (int i = 0; i < n; i++) {
+            yMinY += Math.pow(y[i] - yx[i], 2);
+        }
+
+        yMinY = Math.sqrt(yMinY);
+
+        return yMinY;
+    }
+
     public static void main(String[] args) {
-        int n = 100; // Кількість рівнянь
-        double h = 1.0 / n; // Крок
-        double[] a = new double[n-1];
-        double[] b = new double[n-1];
-        double[] c = new double[n-1];
-        double[] f = new double[n-1];
-        double[] y = new double[n+1];
+        int n = 10;
+        double yMinY = rightSolve(n);
+        System.out.printf("n= %d:\n", n);
+        System.out.printf("||y-y*|| = %.7f\n", yMinY);
 
-        // Обчислення коефіцієнтів
-        for (int i = 0; i < n-1; i++) {
-            double x = (i+1) * h;
-            a[i] = (1.0 / (h*h)) + ((1.0 + x) / (2.0 * h));
-            b[i] = (1.0 / (h*h)) - ((1.0 + x) / (2.0 * h));
-            c[i] = 1.0 + (2.0 / (h*h));
-            f[i] = 2.0 / Math.pow((1.0 + x), 3.0);
-        }
-
-        // Граничні умови
-        y[0] = 1.0;
-        y[n] = 0.5;
-
-        // Прямий хід методу лівої прогонки
-        double[] alpha = new double[n-1];
-        double[] beta = new double[n-1];
-        alpha[0] = -b[0] / c[0];
-        beta[0] = f[0] / c[0];
-        for (int i = 1; i < n-1; i++) {
-            alpha[i] = -b[i] / (a[i-1] * alpha[i-1] + c[i]);
-            beta[i] = (f[i] - a[i-1] * beta[i-1]) / (a[i-1] * alpha[i-1] + c[i]);
-        }
-
-        // Зворотній хід методу лівої прогонки
-        y[n-1] = (f[n-2] - a[n-2] * beta[n-2]) / (c[n-2] + a[n-2] * alpha[n-2]);
-        for (int i = n-2; i >= 0; i--) {
-            y[i+1] = alpha[i] * y[i+2] + beta[i];
-        }
-
-        // Виведення розв'язку
-        System.out.println("x \t y(x) \t y*(x)");
-        for (int i = 0; i <= n; i++) {
-            double x = i * h;
-            System.out.printf("%.4f \t %.4f \t %.4f\n", x, y[i], 1.0 / (x+1.0));
-        }
+//        System.out.println();
+//
+//        n = 100;
+//        yMinY = rightSolve(n);
+//        System.out.printf("n= %d:\n", n);
+//        System.out.printf("||y-y*|| = %.7f\n", yMinY);
     }
 }
